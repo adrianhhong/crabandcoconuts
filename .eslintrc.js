3b183f5d8ea1828e15df7e6eb7a1f02dc568e164
Adrian Hong <adrianhhong@gmail.com>
2021-08-08 02:19:35 +1000

    :building_construction: Rearranged folder structure for Heroku

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+  root: true,
+  env: {
+    node: true,
   },
   extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/prettier',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
   rules: {
     'no-console': 2,
     'prettier/prettier': 2,
