a79c910e3a6fc5cd85841be3d63d5dfd9296f070
Adrian Hong <adrianhhong@gmail.com>
2021-08-07 15:27:10 +1000

    :wrench: Changed server port to work with Heroku

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,6 +1,6 @@
 export const server = {
   socketPort: parseInt(`${process.env.SOCKET_PORT}`, 10) || 5000,
-  apiPort: parseInt(`${process.env.API_PORT}`, 10) || 3000,
+  apiPort: process.env.PORT || 3000,
   logLevel: 'debug',
 };
 
